6734062468ef7d8ab81f8a9d4b5c79af0aab6303
SJ Coder <docsaj@outlook.com>
2022-07-27 19:08:07 +0000

    routes working

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.get("/", (req, res, next) => {
+    res.send("Api running");
+});
+app.use("/api/auth", require("./routers/auth.js"));
+
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
